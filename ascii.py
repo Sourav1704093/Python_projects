90ee3b95cf9db4682f87710165ee86fb1a1a2e12
Sherlocked <42141718+Sourav1704093@users.noreply.github.com>
2021-07-19 00:36:21 +0600

    now you can also create text file of ascii image

--- ascii.py
+++ ascii.py
@@ -1,42 +1,49 @@
-from types import resolve_bases
 import PIL
 
-#create a list of ascii characters to build our output image
-ascii_chars = ["@","#","$","%","?","*","+",",","*",".",":",";"]
+# ascii characters used to build the output text
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
 
-#resize image
-def resize_img(image,new_width = 100):
-    width,height = image.size
+# resize image according to a new width
+def resize_image(image, new_width=100):
+    width, height = image.size
     ratio = height/width
-    new_height = int(new_width*ratio)
-    resize_image = image.resize((new_width,new_height))
-    return resize_image
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return(resized_image)
 
-#now convert the image into a grayscale image
-def to_gray(image):
-    grayscale_img = image.convert("L")
-    return grayscale_img
-
-def pixel_to_ascii(image):
+# convert each pixel to grayscale
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return(grayscale_image)
+    
+# convert pixels to a string of ascii characters
+def pixels_to_ascii(image):
     pixels = image.getdata()
-    characters= "".join([ascii_chars[pixel//25] for pixel in pixels])
-    return characters
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return(characters)    
 
-def main(new_width = 100):
-    #we will attempt to open an image for this project
-    path = input("Enter the image path:")
+def main(new_width=100):
+    # attempt to open image from user-input
+    path = input("Enter a valid pathname to an image:\n")
     try:
         image = PIL.Image.open(path)
     except:
-        print(path," is not valid")
+        print(path, " is not a valid pathname to an image.")
         return
+  
+    # convert image to ascii    
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
     
-    new_img = pixel_to_ascii(to_gray(resize_img(image)))
+    # format
+    pixel_count = len(new_image_data)  
+    ascii_image = "\n".join([new_image_data[index:(index+new_width)] for index in range(0, pixel_count, new_width)])
     
-    #formating method
-    pixel_cnt = len(new_img)
-    ascii_img = "\n".join([new_img[index:(index+new_width)] for index in range(0,pixel_cnt,new_width)])
+    # print result
+    print(ascii_image)
     
-    print(ascii_img)
-
+    # save result to "ascii_image.txt"
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_image)
+ 
+# run program
 main()
